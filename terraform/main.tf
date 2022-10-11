e9fea69fdd9f8848dba95d53d5d2fc32b0cc1c00
Paul Silcock <ps88uk@gmail.com>
2022-10-11 17:19:57 +0100

    Adds google cas issuer & cert-manager to provision certs

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,35 +24,39 @@ resource "google_service_account" "main" {
 }
 
 resource "google_container_cluster" "main" {
-  name     = "${var.cluster_name}"
+  name     = var.cluster_name
   location = var.location
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 1  
+  initial_node_count       = 1
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
 }
 
 resource "google_container_node_pool" "main_spot_nodes" {
-  name               = "${var.cluster_name}-nodepool"
-  location           = var.location
-  cluster            = google_container_cluster.main.name
+  name     = "${var.cluster_name}-nodepool"
+  location = var.location
+  cluster  = google_container_cluster.main.name
 
   initial_node_count = 2
-  
+
   autoscaling {
     min_node_count = 2
     max_node_count = 3
   }
 
   management {
-    auto_repair = true
+    auto_repair  = true
     auto_upgrade = true
   }
 
   node_config {
-    preemptible = true
+    preemptible  = true
     machine_type = "e2-highmem-2"
 
     service_account = google_service_account.main.email
@@ -60,7 +64,7 @@ resource "google_container_node_pool" "main_spot_nodes" {
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-  
+
   timeouts {
     create = "20m"
     update = "20m"
@@ -68,7 +72,7 @@ resource "google_container_node_pool" "main_spot_nodes" {
 }
 
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [google_container_cluster.main]
+  depends_on      = [google_container_cluster.main]
   create_duration = "30s"
 }
 
@@ -89,33 +93,89 @@ provider "kubectl" {
 }
 
 data "kubectl_file_documents" "namespaces" {
-    content = file("../manifests/namespaces.yaml")
-} 
+  content = file("../manifests/namespaces.yaml")
+}
 
 data "kubectl_file_documents" "certs" {
-    content = file("../manifests/certs.yaml")
-} 
+  content = file("../manifests/certs.yaml")
+}
 
 data "kubectl_file_documents" "argocd" {
-    content = file("../manifests/install-argocd.yaml")
-} 
+  content = file("../manifests/install-argocd.yaml")
+}
 
 resource "kubectl_manifest" "namespaces" {
-    count     = length(data.kubectl_file_documents.namespaces.documents)
-    yaml_body = element(data.kubectl_file_documents.namespaces.documents, count.index)
+  count     = length(data.kubectl_file_documents.namespaces.documents)
+  yaml_body = element(data.kubectl_file_documents.namespaces.documents, count.index)
 }
 
 resource "kubectl_manifest" "certs" {
-    count     = length(data.kubectl_file_documents.certs.documents)
-    yaml_body = element(data.kubectl_file_documents.certs.documents, count.index)
+  count     = length(data.kubectl_file_documents.certs.documents)
+  yaml_body = element(data.kubectl_file_documents.certs.documents, count.index)
 }
 
 resource "kubectl_manifest" "argocd" {
-    depends_on = [
-      kubectl_manifest.namespaces,
-      kubectl_manifest.certs
-    ]
-    count     = length(data.kubectl_file_documents.argocd.documents)
-    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
-    override_namespace = "argocd"
+  depends_on = [
+    kubectl_manifest.namespaces,
+    kubectl_manifest.certs
+  ]
+  count              = length(data.kubectl_file_documents.argocd.documents)
+  yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
+  override_namespace = "argocd"
+}
+
+data "kubectl_file_documents" "certmanager" {
+  content = file("../manifests/cert-manager-v1.9.1.yaml")
+}
+
+resource "kubectl_manifest" "certmanager" {
+  count              = length(data.kubectl_file_documents.certmanager.documents)
+  yaml_body          = element(data.kubectl_file_documents.certmanager.documents, count.index)
+  override_namespace = "cert-manager"
+}
+
+data "kubectl_file_documents" "google_cas_issuer" {
+  content = file("../manifests/google-cas-issuer-v0.6.0-alpha.1.yaml")
+}
+
+resource "kubectl_manifest" "google_cas_issuer" {
+  count              = length(data.kubectl_file_documents.google_cas_issuer.documents)
+  yaml_body          = element(data.kubectl_file_documents.google_cas_issuer.documents, count.index)
+  override_namespace = "cert-manager"
+}
+
+resource "google_service_account" "service_account" {
+  account_id = "sa-google-cas-issuer"
+}
+
+module "workload-identity" {
+  source       = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  cluster_name = var.cluster_name
+  name         = "sa-google-cas-issuer"
+  namespace    = "cert-manager"
+  project_id   = var.project_id
+  k8s_sa_name  = "cert-manager-google-cas-issuer"
+}
+
+resource "google_privateca_ca_pool_iam_binding" "binding" {
+  ca_pool  = "ca_pool"
+  project  = var.project_id
+  location = var.region
+  role     = "roles/privateca.certificateManager"
+  members = [
+    "serviceAccount:sa-google-cas-issuer@${var.project_id}.iam.gserviceaccount.com",
+  ]
+}
+
+resource "kubectl_manifest" "certIssuer" {
+  yaml_body = <<YAML
+apiVersion: cas-issuer.jetstack.io/v1beta1
+kind: GoogleCASClusterIssuer
+metadata:
+  name: googlecasclusterissuer-sample
+spec:
+  project: <project_id>
+  location: ${var.region}
+  caPoolId: ca-pool
+YAML
 }
