1c4de43e651527b004119e85903905965e238701
Paul Silcock <ps88uk@gmail.com>
2022-10-12 16:40:39 +0100

    ...

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.15"
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
@@ -130,8 +130,7 @@ provider "kubectl" {
   host                   = module.gke_auth.host
   cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
   token                  = module.gke_auth.token
-  load_config_file       = false
-  apply_retry_count      = 3
+  load_config_file       = true
 }
 
 data "kubectl_file_documents" "namespaces" {
