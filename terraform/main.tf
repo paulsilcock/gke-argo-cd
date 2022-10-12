8448d65ae08b244162ef8b3c94a71d93962a0800
Paul Silcock <ps88uk@gmail.com>
2022-10-12 14:24:28 +0100

    Remove workload identity from cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,10 +32,6 @@ resource "google_container_cluster" "main" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
-
-  workload_identity_config {
-    workload_pool = "${var.project_id}.svc.id.goog"
-  }
 }
 
 resource "google_container_node_pool" "main_spot_nodes" {
