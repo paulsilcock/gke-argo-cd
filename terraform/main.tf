41cbffb2b37e95d6dac1656f797f373a2030286a
Paul Silcock <ps88uk@gmail.com>
2022-10-12 16:44:29 +0100

    Attempts to force fresh auth token

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,25 +113,25 @@ resource "google_container_node_pool" "gpu_spot_nodes" {
 }
 
 resource "time_sleep" "wait_30_seconds" {
-  depends_on      = [google_container_node_pool.gpu_spot_nodes]
+  depends_on      = [google_container_cluster.main]
   create_duration = "30s"
 }
 
-# module "gke_auth" {
-#   depends_on           = [time_sleep.wait_30_seconds]
-#   source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-#   project_id           = var.project_id
-#   cluster_name         = google_container_cluster.main.name
-#   location             = var.location
-#   use_private_endpoint = false
-# }
+module "gke_auth" {
+  depends_on           = [time_sleep.wait_30_seconds]
+  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id           = var.project_id
+  cluster_name         = google_container_cluster.main.name
+  location             = var.location
+  use_private_endpoint = false
+}
 
-# provider "kubectl" {
-#   host                   = module.gke_auth.host
-#   cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
-#   token                  = module.gke_auth.token
-#   load_config_file       = true
-# }
+provider "kubectl" {
+  host                   = module.gke_auth.host
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  token                  = module.gke_auth.token
+  load_config_file       = true
+}
 
 data "kubectl_file_documents" "namespaces" {
   content = file("../manifests/namespaces.yaml")
